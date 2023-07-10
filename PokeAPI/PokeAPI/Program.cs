4a0686dc4f25329c2ef63e41da369e8976d3da31
Ryan Jesus Valentim de Oliveira <73857448+ryanjesus@users.noreply.github.com>
2023-07-10 10:49:53 -0300

    Add files via upload

---
+++ PokeAPI/PokeAPI/Program.cs
@@ -0,0 +1,28 @@
+﻿using RestSharp;
+
+namespace pokeapi
+{
+	class Program
+	{ 
+		static void Main(string[] args)
+		{
+			InvocarGet();
+		}
+
+		private static void InvocarGet()
+		{
+
+			var client = new RestClient($"https://pokeapi.co/api/v2/pokemon/");
+			RestRequest request = new RestRequest("", Method.Get);
+			var response = client.Execute(request);
+
+			if (response.StatusCode == System.Net.HttpStatusCode.OK)
+				Console.WriteLine(response.Content);
+			else
+			{
+				Console.WriteLine(response.ErrorMessage);
+			}
+			Console.ReadKey();
+		}
+	}
+}
